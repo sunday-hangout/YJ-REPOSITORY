985b4b683a898cb2f5526e4530944041b024e631
yeoj1n <yeojinkim94@gmail.com>
2020-03-13 23:41:52 +0900

    해시 > 42576

---
+++ SundayAlgorithm/src/day_200312/pb42576.java
@@ -0,0 +1,29 @@
+package day_200312;
+
+import java.util.Arrays;
+
+//완주하지 못한 선수
+public class pb42576 {
+	public static void main(String[] args) {
+		String str1[] = {"marina", "josipa", "nikola", "vinko", "filipa"};
+		String str2[] = {"josipa", "filipa", "marina", "nikola"};
+		
+		System.out.println(solution(str1, str2));
+	}
+	// participant -1 = completion
+	public static String solution(String[] participant, String[] completion) {
+		Arrays.sort(participant);
+		Arrays.sort(completion);
+		
+		String answer = participant[participant.length - 1];
+		
+		for(int i=0; i<completion.length; i++) {
+			if(!participant[i].equals(completion[i])) {
+				return participant[i];
+			}
+		}
+		
+		return answer;
+	}
+}
+

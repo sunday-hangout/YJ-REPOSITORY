b0c9c29fa59a7611fdfeeaede78505aa6609970e
yeoj1n <yeojinkim94@gmail.com>
2020-03-14 23:34:48 +0900

    0312 commit

---
+++ SundayAlgorithm/src/day_200312/pb42577.java
@@ -0,0 +1,34 @@
+package day_200312;
+
+import java.util.Arrays;
+
+//해시 > 전화번호 목록
+public class pb42577 {
+
+	public static void main(String args[]) {
+		String[] phone_book = {"119", "97674223", "1195524421"};
+		
+		System.out.println(solution(phone_book));
+	}
+	/*
+	 * 전화번호부를 오름차순으로 정렬한 후
+	 * 이중 for문을 사용하여 비교한다.
+	 * startsWith를 사용하여 비교
+	 */
+	public static boolean solution(String[] phone_book) {
+     boolean answer = true;
+     Arrays.sort(phone_book);//정렬
+     
+     if(phone_book.length == 1) return false;
+     
+     for(int i=0; i<phone_book.length-1; i++) {
+     	for(int j=i+1; j<phone_book.length; j++) {
+     		// 정렬한 후이므로 뒤의 단어들에 앞의 단어가 포함되는지만 확인하면 된다.
+     		if(phone_book[j].startsWith(phone_book[i])) return false;
+      	}
+     }
+     
+     return answer;
+ }
+}
+
---
+++ SundayAlgorithm/src/day_200312/pb42588.java
@@ -0,0 +1,22 @@
+package day_200312;
+
+// 스택, 큐 > 탑 
+public class pb42588 { 
+	public static int[] solution(int[] heights) {
+        int[] answer = new int[heights.length];
+        
+        for(int i=heights.length-1; i>=0; i--) {
+        	int temp = heights[i];
+        	int idx = i-1;
+        	
+        	while(idx >=0) {
+        		if(temp < heights[idx]) {
+        			answer[i] = idx+1;
+        			break;
+        		}
+        		idx --;
+        	}
+        }
+        return answer;
+    }
+}

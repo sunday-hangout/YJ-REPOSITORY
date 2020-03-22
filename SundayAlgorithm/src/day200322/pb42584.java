504a2953d5f8d1a362fa6e2eeb1074f781fbf55d
yeoj1n <yeojinkim94@gmail.com>
2020-03-22 10:54:44 +0900

    프로그래머스 > 42584번 중복 코드 제거

--- SundayAlgorithm/src/day200322/pb42584.java
+++ SundayAlgorithm/src/day200322/pb42584.java
@@ -9,10 +9,11 @@ public class pb42584 {
 
 	 public int[] solution(int[] prices) {
 	      int size = prices.length;
-	      int[] answer = new int[size];
+	      int[] answer = new int[size]; // 가격이 유지된 시간을 담은 배열
 	      
 	      int count = 0;
-	      for(int i=0; i<size-1; i++) {
+	      
+	      for(int i=0; i<size-1; i++) { // 두개씩 비교해야하기 때문에 size-1 만큼 돌린다.
 	    	  count = 0;
 	    	  
 	    	  for(int j=i+1; j<size; j++) {
@@ -24,9 +25,8 @@ public class pb42584 {
 	    				  answer[i] = j - i;
 	    				  break;
 	    			  }
-	    		  } else {
-	    			  answer[i] = j - i;
-	    			  count = 0;
+	    		  } else { // 가격이 떨어진 경우
+	    			  answer[i] = j - i; // 유지된 시간 : j-i
 	    			  break;
 	    		  }
 	    	  }

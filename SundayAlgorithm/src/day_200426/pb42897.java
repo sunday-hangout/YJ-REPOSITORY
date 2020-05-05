6f7c5d82ea56789e9a8f439629a39e6e765882c4
yeoj1n <yeojinkim94@gmail.com>
2020-05-05 23:56:24 +0900

    programmers > 42897번 도둑질 commit

--- SundayAlgorithm/src/day_200426/pb42897.java
+++ SundayAlgorithm/src/day_200426/pb42897.java
@@ -11,10 +11,13 @@ public class pb42897 {
     	int cnt = money.length;
     	int[] dp = new int[cnt-1];// 첫번째 집 도둑질 (마지막 집 도둑질 불가능)
     	int[] dp2 = new int[cnt];// 첫번째 집 도둑질x
-    	
-    	
+    
+    	// 첫번째 집 도둑
     	dp[0] = money[0];
     	dp[1] = money[0];// 첫번째 집 훔쳤으니 더해주어야함
+    	
+    	// 두번째 집 도둑
+    	dp2[0] = 0;
     	dp2[1] = money[1];
     	
     	for(int i=2; i<cnt-1;i++){

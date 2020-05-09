e72b2db42ce4f09669bd960d11c017bb0b3a1347
yeoj1n <yeojinkim94@gmail.com>
2020-05-09 10:38:47 +0900

    프로그래머스 > 예

--- SundayAlgorithm/src/day_200405/pb42839.java
+++ SundayAlgorithm/src/day_200405/pb42839.java
@@ -2,75 +2,77 @@ package day_200405;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
 // 소수 찾기
 public class pb42839 {
-
 	public int solution(String numbers) {
-		char[] list = numbers.toCharArray();
-		int[] combArr = new int[list.length];
-		for (int i = 0; i < list.length; i++) {
-			combArr[i] = Integer.parseInt(String.valueOf(list[i]));
-		}
-		Set<Integer> primeList = new HashSet<>();
-		for (int i = 1; i <= list.length; i++) {
-			perm(list, 0, i, primeList);
-		}
+	    int maxNum = makeMaxNum(numbers.toCharArray());
+	    boolean[] primeNum = findPrimeNum(maxNum);
 
-		return primeList.size();
+	    return findMakePossiblePrimeNum(primeNum, maxNum);
+	}
+	
+	// 숫자 카드로 만들 수 있는 가장 큰수 
+	private int makeMaxNum(char[] inputNum) {
+	    Arrays.sort(inputNum);
+	    int len = inputNum.length;
+	    for (int i = 0; i < len/2; i++) {
+	        char temp = inputNum[i];
+	        inputNum[i] = inputNum[len - 1 - i];
+	        inputNum[len - 1 - i] = temp;
+	    }
+	    return Integer.parseInt(new String(inputNum));
 	}
 
-	public void perm(char[] arr, int depth, int k, Set primeList) {
-		if (depth == k) {
-			print(arr, k, primeList);
-			return;
-		} else {
-			for (int i = depth; i < arr.length; i++) {
-				swap(arr, i, depth);
-				perm(arr, depth + 1, k, primeList);
-				swap(arr, i, depth);
-			}
-		}
+	// 제일 큰 수를 찾아 가장 큰 수까지의 소수를 구함
+	private boolean[] findPrimeNum(int maxNum) {
+	    boolean[] result = new boolean[maxNum + 1];
+	    for (int i = 2; i*i<maxNum; i++) {
+	    	for (int j = i * 2; j <= maxNum; j += i) {
+	    		result[j] = true;
+	        }
+	        
+	    }
+	    return result;
 	}
 
-	public void swap(char[] arr, int i, int j) {
-		char temp = arr[i];
-		arr[i] = arr[j];
-		arr[j] = temp;
+	// 숫자 조각으로 만들 수 있는 소수 개수 반환 
+	private int findMakePossiblePrimeNum(boolean[] primeNum, int maxNum) {
+	    int possiblePrimeNumCount = 0;
+
+	    for (int primeNumIndex = 2; primeNumIndex <= maxNum; primeNumIndex++) {
+	        if (!primeNum[primeNumIndex] && isPossible(maxNum, primeNumIndex)) {
+	            possiblePrimeNumCount++;
+	        }
+	    }
+	    return possiblePrimeNumCount;
 	}
+	
+	// 소수가 카드 조합으로 만들 수 있는지  
+	private boolean isPossible(int maxNum, int primeNum) {
+	    int[] numCount = countAvailableNums(maxNum);
 
-	public void print(char[] arr, int k, Set primeList) {
-		StringBuilder a = new StringBuilder();
-		for (int i = 0; i < k; i++) {
-			System.out.print(arr[i]);
-			a.append(arr[i]);
-		}
-		System.out.println();
-		isPrime(primeList, a);
+	    while(primeNum != 0) {
+	        if (numCount[primeNum % 10] <= 0) return false;
+	        numCount[primeNum % 10]--;
+	        primeNum /= 10;
+	    }
+
+	    return true;
 	}
-
-	private void isPrime(Set primeList, StringBuilder a) {
-		int b = Integer.parseInt(String.valueOf(a));
-		boolean prime = true;
-		if (b <= 1) {
-			return;
-		}
-		for (int j = 2; j <= Math.sqrt(b); j++) {
-			if (b % j == 0) {
-				prime = false;
-				break;
-			}
-		}
-		if (prime) {
-			primeList.add(b);
-		}
+	// 숫자 카드에서 사용할 수 있는 숫자를 세는 함수 (0~9) 
+	private int[] countAvailableNums(int maxNum) {
+	    char[] str = String.valueOf(maxNum).toCharArray();
+	    int[] numCount = new int[10];
+	    for (int i = 0; i < str.length; i++) {
+	        numCount[str[i] - '0']++;
+	    }
+	    return numCount;
 	}
-
-
+	
 
 	@Test
 	public void test() {

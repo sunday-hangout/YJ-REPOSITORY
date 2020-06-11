6d3a2921691173969ce99030e265723ebcbee73b
yeoj1n <yeojinkim94@gmail.com>
2020-06-11 23:40:03 +0900

    #12901 solve

--- SundayAlgorithm/src/day_200614/pb12901.java
+++ SundayAlgorithm/src/day_200614/pb12901.java
@@ -1,5 +1,29 @@
 package day_200614;
 
-public class pb12901 {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
+// 2016년 
+public class pb12901 {
+	// 1월 1일 FRI
+	public String solution(int a, int b) {
+		String[] day = new String[] {"FRI","SAT","SUN","MON","TUE","WED","THU"};
+		int[] month = new int[] {0,31,29,31,30,31,30,31,31,30,31,30,31};
+		
+		// 1월1일부터 시작이므로 -1로 시
+		int sum = -1;
+		for(int i=1; i<a; i++) {
+			sum += month[i];
+		}
+		sum += b;
+		sum %= 7;
+		
+		return day[sum];
+	}
+	
+	@Test
+	public void test() {
+		//assertEquals("TUE", solution(5,24));
+	}
 }
